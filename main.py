49a885bd90a1f7b500c2c6248d830a23cbcb4699
jennifer <jennifersantossouza@hotmail.com>
2024-10-11 16:17:49 -0300

    feat: Add the image background and the popup question

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 import turtle
+import pandas
 
 screen = turtle.Screen()
 screen.title("U.S. States Game")
 
+image = "blank_states_img.gif"
+screen.addshape(image)
 
+turtle.shape(image)
 
-
-
+answer = screen.textinput(title="Guess the State", prompt="What's another state's name?")
 
 
 
