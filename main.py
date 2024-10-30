9f9ba9033acb8f95c7976c9ec1049ab045568e71
jennifer <jennifersantossouza@hotmail.com>
2024-10-30 12:50:46 -0300

    feat: Display map and ask for the country

--- main.py
+++ main.py
@@ -9,8 +9,18 @@ screen.addshape(image)
 
 turtle.shape(image)
 
+data = pandas.read_csv("50_states.csv")
+all_states = data.state.to_list()
+
 answer = screen.textinput(title="Guess the State", prompt="What's another state's name?")
 
+if answer in all_states:
+    t = turtle.Turtle()
+    t.hideturtle()
+    t.penup()
+    state_data = data[data.state == answer]
+    t.goto(state_data.x, state_data.y)
+    t.write(state_data.state)
 
 
 screen.exitonclick()
