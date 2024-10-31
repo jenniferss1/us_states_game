b43425d606a9f3e4c2d4e0aa792ca9b1a3e29b8c
jennifer <jennifersantossouza@hotmail.com>
2024-10-31 16:36:03 -0300

    feat: implementing the guessing input

--- main.py
+++ main.py
@@ -11,16 +11,24 @@ turtle.shape(image)
 
 data = pandas.read_csv("50_states.csv")
 all_states = data.state.to_list()
+guessed_states = []
 
-answer = screen.textinput(title="Guess the State", prompt="What's another state's name?")
-
-if answer in all_states:
-    t = turtle.Turtle()
-    t.hideturtle()
-    t.penup()
-    state_data = data[data.state == answer]
-    t.goto(state_data.x, state_data.y)
-    t.write(state_data.state)
-
-
-screen.exitonclick()
+while len(guessed_states) < 50:
+    answer_state = screen.textinput(title=f"{len(guessed_states)}/50 States Correct",
+                                    prompt="What's another state's name?").title()
+    if answer_state == "Exit":
+        missing_states = []
+        for state in all_states:
+            if state not in guessed_states:
+                missing_states.append(state)
+        new_data = pandas.DataFrame(missing_states)
+        new_data.to_csv("states_to_learn.csv")
+        break
+    if answer_state in all_states:
+        guessed_states.append(answer_state)
+        t = turtle.Turtle()
+        t.hideturtle()
+        t.penup()
+        state_data = data[data.state == answer_state]
+        t.goto(state_data.x.item(), state_data.y.item())
+        t.write(answer_state)
